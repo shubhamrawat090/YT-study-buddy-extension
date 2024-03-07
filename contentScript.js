d32567a08ddbe21fc9a00b8440d0c73d6a59d6a9
Grudge69 <shubham.shubham.rawat090@gmail.com>
2024-03-07 22:50:36 +0530

    Observer added for waiting the timestamp to be rendered

--- contentScript.js
+++ contentScript.js
@@ -1,70 +1,31 @@
-console.log("in contentScript.js");
+// Check if it's a Playlist Page
+function isPlaylistPage() {
+  return window.location.href.includes("youtube.com/playlist");
+}
 
-// Create a MutationObserver instance, helps in waiting for a specific element to be loaded
-const observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    // Check if the mutation involves added nodes
-    if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
-      // Iterate through added nodes
-      let timerSpanFound = false;
-      mutation.addedNodes.forEach(function (node) {
-        // Check if the added node matches the target class
-        if (node.classList && node.classList.contains('style-scope') && node.classList.contains('ytd-thumbnail-overlay-time-status-renderer')) {
-          timerSpanFound = true;
-        }
-      });
-
-      // Don't want to calls on each timer being loaded. Just after the last one is loaded
-      if (timerSpanFound) {
-        contentLoaded();
-      }
-    }
-  });
-});
-
-// Start observing mutations in the entire document body
-observer.observe(document.body, { subtree: true, childList: true });
-
-// Handle tab closures and page refreshes
-window.addEventListener('beforeunload', function (event) {
-  // Disconnect the observer to clean up resources
-  observer.disconnect();
-});
-
-function contentLoaded() {
-  // check if a valid YT page
-
-  // BOOKMARKING LOGIC FOR YT VIDEO PAGE
-
-  // TIME CALCULATING LOGIC FOR YT PLAYLIST PAGE
-  // check if a YT playlist page and send the timestamps to background.js
-  // background.js will calculate the time and send back to contentScript
-  // on receiving the calculation the contentScript injects that into the YT playlist page
-
-  const url = window.location.href;
-  if (isYoutubePlaylistUrl(url)) {
-    // get the timestamp spans
-    const timerClass = ".ytd-thumbnail-overlay-time-status-renderer";
-
-    const timerSpans = document.querySelectorAll(timerClass);
-    let count = 0;
-    for (let i = 0; i < timerSpans.length; i++) {
-      const timer = timerSpans[i];
-      if(timer) {
-        console.log(timer.innerText)
-        count++;
-      }
-    }
-
-    console.log("count: ", count);
-
-
-  } else {
-    console.log("Invalid Playlist URL")
+function retrieveTimestamps() {
+  if (isPlaylistPage()) {
+    const videos = document.querySelectorAll("ytd-playlist-video-renderer #content");
+    videos.forEach((video, idx) => {
+      console.log("-----------")
+      console.log(idx+1);
+      const thumbnail = video.querySelector("#overlays.ytd-thumbnail");
+      console.log(thumbnail)
+      const timestamps = thumbnail.querySelector("#time-status.ytd-thumbnail-overlay-time-status-renderer")
+      console.log(timestamps)
+    })
   }
 }
 
-
-function isYoutubePlaylistUrl(url) {
-  return url && url.includes("youtube.com/playlist?");
-}
+const callback = function(mutationsList, observer) {
+  // PROBLEM: This wait for the first timestamp element to render on page and then breaks. I want it to wait for the last one
+  for (const mutation of mutationsList) {
+    if(mutation.target && mutation.target.classList.contains('ytd-thumbnail-overlay-time-status-renderer')) {
+      retrieveTimestamps();
+      observer.disconnect();
+      break;
+    }
+  }
+};
+const observer = new MutationObserver(callback);
+observer.observe(document.body, {childList: true, subtree: true });
