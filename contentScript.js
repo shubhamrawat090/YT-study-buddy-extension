d2dc472b15330197803b649d5014136b26f3eaee
Grudge69 <shubham.shubham.rawat090@gmail.com>
2024-03-14 23:24:05 +0530

    Finishes up the content HTML injection on the playlist page

--- contentScript.js
+++ contentScript.js
@@ -1,47 +1,78 @@
+let totalTimeInSec = 0;
+let videosCounted = 0;
+let videosNotCounted = 0;
+
 // Check if it's a Playlist Page
 function isPlaylistPage() {
   return window.location.href.includes("youtube.com/playlist");
 }
 
 function retrieveTimestamps() {
+  const videos = document.querySelectorAll(
+    "ytd-playlist-video-renderer #content"
+  );
 
-  const videos = document.querySelectorAll("ytd-playlist-video-renderer #content");
-  let daySum = 0, hourSum = 0, minSum = 0, secSum = 0;
+  totalTimeInSec = 0;
+  videosCounted = 0;
+  videosNotCounted = 0;
+
   videos.forEach((video, idx) => {
-    console.log("-----------")
-    console.log(idx + 1);
     if (!video) return;
     const thumbnail = video.querySelector("#overlays.ytd-thumbnail");
 
     if (!thumbnail) return;
-    const timestamps = thumbnail.querySelector("#time-status.ytd-thumbnail-overlay-time-status-renderer")
+    const timestamps = thumbnail.querySelector(
+      "#time-status.ytd-thumbnail-overlay-time-status-renderer"
+    );
 
     if (!timestamps) return;
-    const timerspan = timestamps.querySelector("#text.ytd-thumbnail-overlay-time-status-renderer");
+    const timerspan = timestamps.querySelector(
+      "#text.ytd-thumbnail-overlay-time-status-renderer"
+    );
 
     if (!timerspan) return;
     const timer = timerspan.innerText;
-    console.log(timer);
     /* 
       Timer format: 10:37 ==> min:sec, 1:10:35 ==> hour:min:sec
     */
     if (!timer) return;
-    const timerSplit = timer.split(':');
+    const timerSplit = timer.split(":");
     if (timerSplit.length === 3) {
       const [hr, min, sec] = timerSplit;
-      hourSum += Number(hr);
-      minSum += Number(min);
-      secSum += Number(sec);
+      totalTimeInSec += Number(hr) * 60 * 60;
+      totalTimeInSec += Number(min) * 60;
+      totalTimeInSec += Number(sec);
     } else {
       const [min, sec] = timerSplit;
-      minSum += Number(min);
-      secSum += Number(sec);
+      totalTimeInSec += Number(min) * 60;
+      totalTimeInSec += Number(sec);
     }
-  })
+  });
 
+  /* 
+  TODO: 
+  Get no. of videos counted here
+  Total number of videos
+  Get no. of videos NOT counted (diff of above 2)
+  Give a message "Scroll down to count more videos" if no. of NOT counted !== 0
+  */
+
+  const metaDataStatsElem = document.querySelector(".metadata-stats");
+  const videoCount = metaDataStatsElem.querySelector(
+    ".yt-formatted-string"
+  )?.innerText;
+
+  let totalVideos = Number(videoCount);
+  videosCounted = videos.length;
+  videosNotCounted = totalVideos - videosCounted;
+
+  injectDataToWebpage();
+}
+
+function secToDayHrMinSec(sec) {
   // Steps for 49hr, 120min, 92 sec  ==> 2 days, 3 hr, 3 min, 2 sec
-  // handle seconds only 
-  // -> 49hr, (120 + 3) min, 2 sec 
+  // handle seconds only
+  // -> 49hr, (120 + 3) min, 2 sec
   // -> 49hr, 123 min, 2 sec
 
   // handle minutes only
@@ -51,33 +82,138 @@ function retrieveTimestamps() {
   // handle hours only
   // -> (0 + 2) days, 3 hr, 3 min, 2 sec
   // -> 2 days, 3 hr, 3 min, 2 sec
+  let min = 0;
+  let hrs = 0;
+  let days = 0;
 
-  minSum += Math.floor(secSum / 60);
-  secSum = secSum % 60;
+  min += Math.floor(sec / 60);
+  sec = sec % 60;
 
-  hourSum += Math.floor(minSum / 60);
-  minSum = minSum % 60;
+  hrs += Math.floor(min / 60);
+  min = min % 60;
 
-  daySum += Math.floor(hourSum / 24);
-  hourSum = hourSum % 24;
+  days += Math.floor(hrs / 24);
+  hrs = hrs % 24;
+
+  return { days, hrs, min, sec };
+}
+
+function injectDataToWebpage() {
+  const metaDataDiv = document.querySelector(".metadata-wrapper");
+  const elemPresent = metaDataDiv.querySelector("#yt-study-buddy-stats");
+  if (elemPresent) {
+    metaDataDiv.removeChild(elemPresent);
+  }
+
+  // Duration at normal speed
+  var { days, hrs, min, sec } = secToDayHrMinSec(totalTimeInSec);
+
+  let daySum = days;
+  let hourSum = hrs;
+  let minSum = min;
+  let secSum = sec;
+
+  // Create an array to store non-empty elements
+  const elements = [];
+
+  // Push non-empty elements into the array
+  if (daySum !== 0) elements.push(`<div>${daySum} days</div>`);
+  if (hourSum !== 0) elements.push(`<div>${hourSum} hours</div>`);
+  if (minSum !== 0) elements.push(`<div>${minSum} mins</div>`);
+  if (secSum !== 0) elements.push(`<div>${secSum} sec</div>`);
 
   console.log({ daySum, hourSum, minSum, secSum });
+  console.log({ videosCounted, videosNotCounted });
 
-  /* 
-    TODO: 
-      Get no. of videos counted here
-      Total number of videos
-      Get no. of videos NOT counted (diff of above 2)
-      Give a message "Scroll down to count more videos" if no. of NOT counted !== 0
-  */
+  // Duration at 1.5x
+  let secAt1_5x = Math.ceil(totalTimeInSec / 1.5);
+  var { days, hrs, min, sec } = secToDayHrMinSec(secAt1_5x);
+
+  let daySumAt1_5x = days;
+  let hourSumAt1_5x = hrs;
+  let minSumAt1_5x = min;
+  let secSumAt1_5x = sec;
+
+  // Create an array to store non-empty elements
+  const elementsAt1_5x = [];
+
+  // Push non-empty elements into the array
+  if (daySumAt1_5x !== 0)
+    elementsAt1_5x.push(`<div>${daySumAt1_5x} days</div>`);
+  if (hourSumAt1_5x !== 0)
+    elementsAt1_5x.push(`<div>${hourSumAt1_5x} hours</div>`);
+  if (minSumAt1_5x !== 0)
+    elementsAt1_5x.push(`<div>${minSumAt1_5x} mins</div>`);
+  if (secSumAt1_5x !== 0) elementsAt1_5x.push(`<div>${secSumAt1_5x} sec</div>`);
+
+  // Duration at 2x
+  let secAt2x = Math.ceil(totalTimeInSec / 2);
+  var { days, hrs, min, sec } = secToDayHrMinSec(secAt2x);
+
+  let daySumAt2x = days;
+  let hourSumAt2x = hrs;
+  let minSumAt2x = min;
+  let secSumAt2x = sec;
+
+  // Create an array to store non-empty elements
+  const elementsAt2x = [];
+
+  // Push non-empty elements into the array
+  if (daySumAt2x !== 0) elementsAt2x.push(`<div>${daySumAt1_5x} days</div>`);
+  if (hourSumAt2x !== 0) elementsAt2x.push(`<div>${hourSumAt2x} hours</div>`);
+  if (minSumAt2x !== 0) elementsAt2x.push(`<div>${minSumAt2x} mins</div>`);
+  if (secSumAt2x !== 0) elementsAt2x.push(`<div>${secSumAt2x} sec</div>`);
+
+  // Construct reqdDiv with non-empty elements
+  const reqdDiv = `
+    <div id="yt-study-buddy-stats">
+      <div class="duration">
+        ${elements.join("")}
+      </div>
+
+      <table class="durationTable">
+        <tr>
+          <td>At 1.5x</td>
+          <td>
+            <div class="durationAt1_5x">
+            ${elementsAt1_5x.join("")}
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>At 2x</td>
+          <td>
+            <div class="durationAt2x">
+            ${elementsAt2x.join("")}
+            </div>
+          </td>
+        </tr>
+      </table>
+      
+      <div class="videosCounted">Videos Counted: ${videosCounted}</div>
+      <div class="videosNotCounted">Videos NOT Counted: ${videosNotCounted}</div>
+      ${
+        videosNotCounted !== 0
+          ? `<div class="scroll-msg">Scroll down to count more videos</div>`
+          : ""
+      }
+    </div>
+    `;
+
+  metaDataDiv.children[0].insertAdjacentHTML("afterend", reqdDiv);
 }
 
 if (isPlaylistPage()) {
   const callback = function (mutationsList, observer) {
     // PROBLEM: This wait for the first timestamp element to render on page and then breaks. I want it to wait for the last one
-    // FIX: Do observe.disconnect() to on beforeunload event
+    // FIX: Do observe.disconnect() in the beforeunload event
     for (const mutation of mutationsList) {
-      if (mutation.target && mutation.target.classList.contains('ytd-thumbnail-overlay-time-status-renderer')) {
+      if (
+        mutation.target &&
+        mutation.target.classList.contains(
+          "ytd-thumbnail-overlay-time-status-renderer"
+        )
+      ) {
         retrieveTimestamps();
         break;
       }
@@ -86,7 +222,7 @@ if (isPlaylistPage()) {
   const observer = new MutationObserver(callback);
   observer.observe(document.body, { childList: true, subtree: true });
 
-  window.addEventListener('beforeunload', function (event) {
+  window.addEventListener("beforeunload", function (event) {
     // Disconnect the observer when the tab is refreshed or closed
     observer.disconnect();
   });
